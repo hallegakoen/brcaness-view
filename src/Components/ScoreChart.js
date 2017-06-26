d10cbc00ae8f7c203d920bfc1850477dc3a1659f
Hannah Allegakoen <hallegakoen@gmail.com>
2017-06-26 14:35:26 -0700

    clean up map methods and reorganize

--- src/Components/ScoreChart.js
+++ src/Components/ScoreChart.js
@@ -1,38 +1,44 @@
-import { Panel} from 'react-bootstrap';
-import React, { Component } from 'react';
+import {Panel} from 'react-bootstrap';
+import React, {Component} from 'react';
 import tcgaData from './tcgaData.json';
 import {VictoryBar, VictoryAxis, VictoryChart, VictoryLine, VictoryLabel} from 'victory';
 import * as d3 from "d3-array";
 
+const getCancerScores = function(cancer, score) {
+  const cancerData = tcgaData.filter(function(e) {return e.cancer === cancer});
+  const cancerScores = cancerData.map((e) => e[score]);
+  return cancerScores;
+}
+
+// format data for VictoryBar
+const getChartData = function(cancer, score) {
+  const histogram = d3.histogram()(getCancerScores(cancer, score));
+  let chartData = histogram.map(function(i) {
+    return({
+              score: (i.x0 + i.x1)/2,
+              frequency: i.length
+    });
+  });
+  return chartData;
+}
+
+// get y-coordinates for patient score vline
+const getLineLength = function(cancer, score) {
+  const chartData = getChartData(cancer, score);
+  let scoreFrequencies = chartData.map (function(i) {return (i.frequency)});
+  const lineLength = (Math.max.apply(null,scoreFrequencies) * 1.2);
+  return lineLength;
+}
+
 class ScoreChart extends Component {
 
-
-  getCancerScores(cancer) {
-    const cancerData = tcgaData.filter(function(e) {return e.cancer === cancer}, this);
-    const cancerScores = cancerData.map(function(e) {return e[this.props.scoreName]}, this);
-    return cancerScores;
-  }
-
-  // format data for VictoryBar
-  getChartData(cancer) {
-    const histogram = d3.histogram()(this.getCancerScores(cancer));
-    let chartData = [];
-    histogram.map(function(i) {
-      chartData.push({
-                score: (i.x0 + i.x1)/2,
-                frequency: i.length
-      });
-    });
-    return chartData;
-  }
-
-  //supposed to make VictoryBar more histogram-ish by removing gaps between bars
+  //make VictoryBar more histogram-ish by removing gaps between bars
   getBinWidth(cancer) {
-    const targetHistogram = d3.histogram()(this.getCancerScores(cancer));
+    const targetHistogram = d3.histogram()(getCancerScores(cancer, this.props.scoreName));
     const targetMin = targetHistogram[0].x0;
     const targetMax =  targetHistogram[targetHistogram.length - 1].x1;
 
-    const referenceHistogram = d3.histogram()(this.getCancerScores('OV'));
+    const referenceHistogram = d3.histogram()(getCancerScores('OV', this.props.scoreName));
     const referenceMax =  referenceHistogram[referenceHistogram.length - 1].x1;
     const referenceMin = referenceHistogram[0].x0;
 
@@ -42,18 +48,9 @@ class ScoreChart extends Component {
     return binwidth;
   }
 
-  // get y-coordinates for patient score vline
-  getLineLength(cancer) {
-    let scoreFrequencies = [];
-    const chartData = this.getChartData(cancer);
-    chartData.map (function(i) {scoreFrequencies.push(i.frequency)});
-    const lineLength = (Math.max.apply(null,scoreFrequencies) * 1.2);
-    return lineLength;
-  }
-
 //update if score changes or if cancer type changes to a valid tcga entry
   shouldComponentUpdate(nextProps, nextState) {
-    let chartData = this.getChartData(nextProps.targetCancer);
+    let chartData = getChartData(nextProps.targetCancer, this.props.scoreName);
     return (
       this.props.patientScore !== nextProps.patientScore
       || chartData.length !== 1
@@ -71,10 +68,9 @@ class ScoreChart extends Component {
           bsStyle = "primary">
           <VictoryChart>
             <VictoryBar
-              data= {this.getChartData(this.props.targetCancer)}
+              data= {getChartData(this.props.targetCancer, this.props.scoreName)}
               x="score"
               y="frequency"
-              interpolation="step"
               style={{
                 data: {fill:"navajowhite",
                        width:350 * this.getBinWidth(this.props.targetCancer),
@@ -82,7 +78,7 @@ class ScoreChart extends Component {
               }}
         />
             <VictoryBar
-              data= {this.getChartData('OV')}
+              data= {getChartData('OV', this.props.scoreName)}
               x="score"
               y= {(d) => -(d.frequency)}
               style={{
@@ -101,8 +97,8 @@ class ScoreChart extends Component {
             />
             <VictoryLine
              data = {[
-               {x:this.props.patientScore, y:-this.getLineLength('OV')},
-               {x:this.props.patientScore, y:this.getLineLength(this.props.targetCancer)}
+               {x:this.props.patientScore, y:-getLineLength('OV', this.props.scoreName)},
+               {x:this.props.patientScore, y:getLineLength(this.props.targetCancer, this.props.scoreName)}
              ]}
             />
           </VictoryChart>
